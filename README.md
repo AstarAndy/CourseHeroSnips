6f31d212011809f04326cb32258422edd8f0dae1
Andrew Faust <ArjfAndy@Gmail.com>
2020-07-25 14:39:41 -0500

    Simple BubbleSort Example

--- README.md
+++ README.md
@@ -4,4 +4,7 @@
 You are free to clone/copy the code in this repo as nothing is copyrighted.  The purpose of the repo is to illustrate basic Java 8+ functionality and also
 to answer specific questions posted on [CourseHero](https://www.coursehero.com)
 
+[BubbleSort](./src/com/homework/BubbleSort.java)
+
+
 
--- src/com/homework/BubbleSort.java
+++ src/com/homework/BubbleSort.java
@@ -24,12 +24,13 @@ public class BubbleSort {
 		// First create an unordered array of ints
 		int unOrderedArray[] = {30, 80, 5, 50, 40, 90, 70, 60, 100, 20};
 		int orderedArray[] = sort(unOrderedArray);
+		int foundAt = search(60, orderedArray);
 		
 		System.out.println("The unordered array is:");
 		printArray(unOrderedArray);
 		System.out.println("The ordered array is:");
 		printArray(orderedArray);
-		
+		System.out.println("Located value 60 at position " + foundAt + " in the reordered array" );
 		
 	}
 	
@@ -61,6 +62,39 @@ public class BubbleSort {
 		
 		return result;
 
+	}
+	
+	/**
+	 * Search a sorted array for the supplied value
+	 * and return the position of the array the value
+	 * is located at or -1 if the value was not in the array.
+	 * Going to use a simple split search and either start at the begining
+	 * or the middle of the array
+	 * @param searchValue int what value to look for
+	 * @param arrayToSearch The integer array to search
+	 * @return foundAt int The position in the array the value was found it
+	 */
+	private int search(int searchValue, int[] arrayToSearch) {
+		int foundAt = -1;
+		
+		int startAt = (arrayToSearch.length / 2);
+		// if the value in the middle of the array is < the
+		// search value then we'll start at the beginning or
+		// we'll start at the computed value
+		
+		if (searchValue < arrayToSearch[startAt]) {
+			startAt = 0;
+		}
+		
+		for (int a = startAt; a < arrayToSearch.length-1; a++) {
+			if (searchValue == arrayToSearch[a]) {
+				foundAt = a;
+				break;
+			}
+		}
+		
+		return foundAt;
+		
 	}
 	
 	public void printArray(int[] arrayToPrint) {
